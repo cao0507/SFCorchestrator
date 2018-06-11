48398a17579115508efe035276c07bc6cf6f59e4
shyung <634125750@qq.com>
2018-06-11 14:12:26 +0800

    simple orchestration

--- orchestrator.py
+++ orchestrator.py
@@ -1,1 +1,20 @@
-import
+def orchestrate(vnfs, cons):
+    sfc = []
+    for vnf in vnfs:
+        if vnf not in sfc:
+            sfc.append(vnf)
+            for i in range(len(cons)):
+                if vnf == cons[i][0]:
+                    counter = 0
+                    while cons[i][1] in sfc:
+                        sfc.remove(cons[i][1])
+                        counter += 1
+                    for j in range(counter):
+                        sfc.append(cons[i][1])
+        else:
+            for k in range(len(sfc)):
+                if sfc[k] == vnf:
+                    sfc.insert(k, vnf)
+                    break
+    return sfc
+
